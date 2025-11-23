5dc822ea8aa163f030b8369124b4e7b4f54efa3d
Kursad26 <108861197+Kursad26@users.noreply.github.com>
2025-11-23 10:41:20 +0300

    Deleted 20251025120000_fix_field_training_settings_rls.sql

--- supabase/migrations/20251116000000_add_time_loss_minutes_to_noi_requests.sql
+++
@@ -1,7 +0,0 @@
--- Add time_loss_minutes column to noi_requests table
-ALTER TABLE noi_requests 
-ADD COLUMN IF NOT EXISTS time_loss_minutes integer;
-
--- Add comment for documentation
-COMMENT ON COLUMN noi_requests.time_loss_minutes IS 'Kayıp Zaman (Dk) - Optional field for lost time in minutes';
-
