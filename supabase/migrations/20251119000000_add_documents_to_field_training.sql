b852c5a900e2cef933cd7879c70c7d19b1f5ad71
Kursad26 <108861197+Kursad26@users.noreply.github.com>
2025-11-28 21:33:50 +0300

    Deleted 20251023053436_fix_manufacturing_activity_control_rls_policies.sql

--- supabase/migrations/20251119000000_add_documents_to_field_training.sql
+++
@@ -1,8 +0,0 @@
--- Add documents column to field_training_reports table
--- This allows users to upload training documents (Word, PDF, PowerPoint)
--- Documents are stored in Cloudinary under 'egitim-dokumanlari' folder
-
-ALTER TABLE field_training_reports
-ADD COLUMN IF NOT EXISTS documents text[] DEFAULT '{}';
-
-COMMENT ON COLUMN field_training_reports.documents IS 'Array of Cloudinary URLs for training documents (Word, PDF, PPT)';
